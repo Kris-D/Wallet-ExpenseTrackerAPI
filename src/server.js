9d7e6745452891c9dafbc49e724a372c230beeb9
Christopher Isioma Dunkwu <chriscjd001@gmail.com>
2025-10-29 15:07:28 +0100

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,14 +3,21 @@ import dotenv from "dotenv/config";
 import { initDB } from "./config/db.js";
 import transactionRoutes from "./routes/transactionsRoute.js";
 import rateLimiter from "./middleWare/rateLimiter.js";
+import job from "./config/cron.js";
 
 const app = express();  
+
+if (process.env.NODE_ENV === "production") job.start();
 
 // Middleware
 app.use(rateLimiter);     
 app.use(express.json());
 
 // Routes 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/api/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5001;
